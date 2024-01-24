94a9a952d7aa3412706dda214584af3a150fd42a
AGRIM140 <71631748+AGRIM140@users.noreply.github.com>
2024-01-24 23:57:17 +0530

    Delete weather app - Copy/weatherApp-Reactjs directory

--- weather app - Copy/weatherApp-Reactjs/src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "{a135b50beab09c7b73d0b48abd9cdeb1}",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
